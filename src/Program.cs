a83c1e4036408cf023f582b710711af7632c0d87
Somayah-Absi <somayahahmedabsi@gmail.com>
2024-03-22 20:10:16 +0300

    all challenge

--- src/Program.cs
+++ src/Program.cs
@@ -11,29 +11,29 @@ namespace FunctionChallenges
             StringNumberProcessor("Hello", 100, 200, "World"); // Expected outcome: "Hello World; 300"
 
             // Challenge 2: Object Swapper
-            // Console.WriteLine("\nChallenge 2: Object Swapper");
-            // int num1 = 25, num2 = 30;
-            // int num3 = 10, num4 = 30;
-            // string str1 = "HelloWorld", str2 = "Programming";
-            // string str3 = "Hi", str4 = "Programming";
+            Console.WriteLine("\nChallenge 2: Object Swapper");
+            int num1 = 25, num2 = 30;
+            int num3 = 10, num4 = 30;
+            string str1 = "HelloWorld", str2 = "Programming";
+            string str3 = "Hi", str4 = "Programming";
                           
-            // SwapObjects(ref ref  num1,refref  num2); // Expected outcome: num1 = 30, num2 = 25  
-            // SwapObjects( ref ref num3, refref num4); // Error: Value must be more than 18
+            SwapObjects( ref  num1,ref  num2); // Expected outcome: num1 = 30, num2 = 25  
+            SwapObjects( ref num3, ref num4); // Error: Value must be more than 18
 
-            // SwapObjects(ref ref str1,ref ref  str2); // Expected outcome: str1 = "Programming", str2 = "HelloWorld"
-            // SwapObjects(ref ref str3, ref ref str4); // Error: Length must be more than 5
+            SwapObjects(ref  str1,ref  str2); // Expected outcome: str1 = "Programming", str2 = "HelloWorld"
+            SwapObjects(ref  str3, ref  str4); // Error: Length must be more than 5
 
-            // SwapObjects( true, false); // Error: Upsupported data type
-            // SwapObjects( num1, str1); // Error: Objects must be of same types
+            SwapObjects( true, false); // Error: Upsupported data type
+            SwapObjects( num1, str1); // Error: Objects must be of same types
 
-            // Console.WriteLine($"Numbers: {num1}, {num2}");
-            // Console.WriteLine($"Strings: {str1}, {str2}");
+            Console.WriteLine($"Numbers: {num1}, {num2}");
+            Console.WriteLine($"Strings: {str1}, {str2}");
             
 
             // Challenge 3: Guessing Game
-            // Console.WriteLine("\nChallenge 3: Guessing Game");
+            Console.WriteLine("\nChallenge 3: Guessing Game");
             
-            // GuessingGame(); // Expected outcome: User input until the correct number is guessed or user inputs `Quit`
+            GuessingGame(); // Expected outcome: User input until the correct number is guessed or user inputs `Quit`
 
             //Challenge 4: Simple Word Reversal
             Console.WriteLine("\nChallenge 4: Simple Word Reversal");
@@ -41,6 +41,61 @@ namespace FunctionChallenges
             string reversed = ReverseWords(sentence);
             Console.WriteLine(reversed); // Expected outcome: "sihT si eht lanigiro !ecnetnes"
         }
+
+         static void StringNumberProcessor(params object[] inputs)
+        {
+            string concatenate = "";
+            int sum = 0;
+            foreach (var input in inputs) {
+                if (input is string)
+                {
+                   concatenate += (string)input + " ";
+                }
+                else {
+                    sum += (int)input;
+
+                }
+
+
+             }
+            concatenate= concatenate.Trim();
+        Console.WriteLine($"{concatenate}; {sum}");
+            
+        }
+        
+        public static void SwapObjects(ref int num1, ref int num2)
+        {
+            if (num1 > 18 && num2 > 18)
+            {
+                (num1, num2) = (num2, num1);
+            }
+            else { 
+                Console.WriteLine("Error: Value must be more than 18");
+            }
+        }
+        public static void SwapObjects(ref string str1,ref string str2) {
+            if (str1.Length > 5 && str2.Length > 5)
+            {
+                (str1, str2) = (str2, str1);
+            }
+            else { 
+             Console.WriteLine("Error: Length must be more than 5");
+
+            }
+
+        }
+        static void SwapObjects( object obj1, object obj2)
+    {
+        if (obj1.GetType() != obj2.GetType())
+        {
+            Console.WriteLine("Error: Objects must be of the same types (string or number)");
+        }
+        else
+        {
+            Console.WriteLine("Error: Unsupported type");
+        }
+    }
+
 
          static void GuessingGame()
         {
@@ -94,30 +149,7 @@ return reversedSentence;
 
     }
 
-        // private static void SwapObjects(ref int num1, ref int num2)
-        // {
-        //     throw new NotImplementedException();
-        // }
-
-         static void StringNumberProcessor(params object[] inputs)
-        {
-            string concatenate = "";
-            int sum = 0;
-            foreach (var input in inputs) {
-                if (input is string)
-                {
-                   concatenate += (string)input + " ";
-                }
-                else {
-                    sum += (int)input;
-
-                }
-
-
-             }
-            concatenate= concatenate.Trim();
-        Console.WriteLine($"{concatenate}; {sum}");
-            
-        }
+        
+        
     }
 }
